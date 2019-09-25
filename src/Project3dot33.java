fa54fb791c4df819779a37ae566fe6d6c81f7479
Kasper7 <kasp119e@easv365.dk>
2019-09-25 14:13:10 +0200

    Initial commit

---
+++ src/Project3dot33.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Project3dot33 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double dinnerPrice;
+
+        int satisfactionLvl;
+
+        double totalPrice;
+
+    }
+}
